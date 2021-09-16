55d732dc99baf14ef1d1424cd9063a31403d47d6
Данил <dan.kesov.2015@gmail.com>
2021-09-16 15:45:10 +0300

    Layering app#4 - It finally works!

--- src/main/java/servlet/TODOServlet.java
+++ src/main/java/servlet/TODOServlet.java
@@ -3,6 +3,7 @@ package servlet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import launch.model.Task;
 import launch.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,6 +15,8 @@ import java.util.Map;
 import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,28 +28,36 @@ import javax.servlet.http.HttpServletResponse;
 )
 
 
-public class HelloServlet extends HttpServlet {
+public class TODOServlet extends HttpServlet {
 
     private int id = 0;
     private final ObjectMapper mapper = new ObjectMapper();
     private final Map<Integer, Task> taskMap = new HashMap<>();
+    private TaskService taskService;
 
-    private ApplicationContext context = new ClassPathXmlApplicationContext("Application.xml");
-
-    private  TaskService taskService=context.getBean(TaskService.class);
-
-
-
-    public HelloServlet(TaskService taskService) {
+    public TODOServlet(TaskService taskService) {
         this.taskService = taskService;
     }
-    private HelloServlet() {
+
+    public TODOServlet() {
+    }
+
+
+    public void setTaskService(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        ApplicationContext context = new ClassPathXmlApplicationContext("Application.xml");
+        setTaskService(context.getBean(TaskService.class));
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        mapper.writeValue(resp.getOutputStream(), taskService.getAll());
+        mapper.writeValue(resp.getOutputStream(), taskService.getAll().values());
     }
 
     @Override
